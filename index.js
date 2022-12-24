ae13ea1ebeef351b389931597cecc9ff74d8c2c9
Desapressado <59629875+Desapressad0x@users.noreply.github.com>
2022-12-23 22:01:23 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Baixar Vídeos
+// @version      0.1
+// @description  Baixar vídeos protegidos do telegram
+// @author       Desapressado
+// @match        https://web.telegram.org/*
+// @icon         https://www.google.com/s2/favicons?sz=75&domain=web.telegram.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if(window.location.href.indexOf('https://web.telegram.org/z') !== -1) {
+      window.location.href = window.location.href.replace('z', 'k');
+    }
+
+    document.addEventListener('keydown', function(event) {
+      if(event.key === 'F2') {
+      try {
+        if(document.querySelector('video').getAttribute('src') !== null) {
+          window.open('https://web.telegram.org/k/' + document.querySelector('video').getAttribute('src'), '_blank');
+        };
+       } catch {}
+     }
+  });
+})();
